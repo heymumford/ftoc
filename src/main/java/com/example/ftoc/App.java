8664cb426c31538d2a0c2385545b130d02f14788
Eric Mumford <ericmumford@outlook.com>
2024-10-18 11:22:00 -0400

    Initial commit of ftoc utility

---
+++ src/main/java/com/example/ftoc/App.java
@@ -0,0 +1,13 @@
+package com.example.ftoc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
